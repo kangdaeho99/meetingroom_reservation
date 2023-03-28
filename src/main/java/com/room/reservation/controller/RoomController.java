f0e7cec124b3365d977e87e1e14411dda54e2c60
daeho kang <eoghdhdls@naver.com>
2023-03-29 06:52:26 +0900

    [ADD] PageServiceTests, PageResultDTO, RoomController 완성

--- src/main/java/com/room/reservation/controller/RoomController.java
+++ src/main/java/com/room/reservation/controller/RoomController.java
@@ -1,18 +1,36 @@
 package com.room.reservation.controller;
 
 
+import com.room.reservation.dto.PageRequestDTO;
+import com.room.reservation.service.RoomService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/room")
 @Log4j2
+@RequiredArgsConstructor
 public class RoomController {
-    @GetMapping({"","/","/list"})
-    public String list(){
+
+    private final RoomService service; //final 로 선언
+
+//    @GetMapping({"","/","/list"})
+    @GetMapping({"","/"})
+    public String index(){
         log.info("list.............");
-        return "room/list";
+//        return "room/list";
+        return "redirect:/room/list";
     }
+
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model){
+        log.info("list..............." + pageRequestDTO);
+        model.addAttribute("result", service.getList(pageRequestDTO));
+    }
+
+
 }
--- src/test/java/com/room/reservation/service/RoomServiceTests.java
+++ src/test/java/com/room/reservation/service/RoomServiceTests.java
@@ -33,8 +33,8 @@ public class RoomServiceTests {
 
     /*********
      *
-     * @Author 강대호
-     * @Description
+     * Author 강대호
+     * Description
      * PageRequestDTO를 이용하기 때문에 생성할때는 1페이지부터 처리하고,
      * 정렬은 상황에 맞게 Sort객체를 생성해서 전달.
      * 테스트 코드의 결과를 보면 Page<Room>이 List<RoomDTO>로 변환되어
@@ -43,7 +43,7 @@ public class RoomServiceTests {
      ********/
     @Test
     public void testList(){
-        IntStream.rangeClosed(1, 50).forEach(i ->{
+        IntStream.rangeClosed(1, 102).forEach(i ->{
             Room room = Room.builder()
                     .title("Title..."+i)
                     .content("Content..."+i)
@@ -58,8 +58,32 @@ public class RoomServiceTests {
                 .build();
 
         PageResultDTO<RoomDTO, Room> resultDTO = service.getList(pageRequestDTO);
+        
+//        PREV: false 현재 1페이지이니 PREV가 없어야됨
+//        NEXT: true 현재 총 102페이지이니 NEXT있어야됨
+//        TOTAL: 11 102개이니 총 11페이지까지 존재, 전체페이지의 개수
+        System.out.println("PREV: "+resultDTO.isPrev());
+        System.out.println("NEXT: "+resultDTO.isNext());
+        System.out.println("TOTAL: "+resultDTO.getTotalPage());
+        System.out.println("=======================================");
+
         for(RoomDTO roomDTO : resultDTO.getDtoList()){
             System.out.println(roomDTO);
         }
+//        ======================================= 화면에 출력될 페이지번호
+//        1
+//        2
+//        3
+//        4
+//        5
+//        6
+//        7
+//        8
+//        9
+//        10
+        System.out.println("=======================================");
+        resultDTO.getPageList().forEach(i -> System.out.println(i));
+
+
     }
 }
