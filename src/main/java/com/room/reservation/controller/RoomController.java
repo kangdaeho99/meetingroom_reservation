99b3f7a21a2bec4644070581d90415531bb8b66b
daeho kang <eoghdhdls@naver.com>
2023-04-01 15:12:55 +0900

    [ADD] room list 실제로 등록완료 그리고 디자인 변경점들 추가

--- src/main/java/com/room/reservation/controller/RoomController.java
+++ src/main/java/com/room/reservation/controller/RoomController.java
@@ -2,13 +2,16 @@ package com.room.reservation.controller;
 
 
 import com.room.reservation.dto.PageRequestDTO;
+import com.room.reservation.dto.RoomDTO;
 import com.room.reservation.service.RoomService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/room")
@@ -31,6 +34,21 @@ public class RoomController {
         log.info("list..............." + pageRequestDTO);
         model.addAttribute("result", service.getList(pageRequestDTO));
         System.out.println("result:::::::::::::::::"+ model.getAttribute("result"));
+    }
+
+    @GetMapping("/register")
+    public void register(){
+        log.info("register get ...");
+    }
+
+    @PostMapping("/register")
+    public String registerPost(RoomDTO dto, RedirectAttributes redirectAttributes){
+        log.info("dto..."+ dto);
+
+        //새로 추가된 엔티티의 번호
+        Long gno = service.register(dto);
+        redirectAttributes.addFlashAttribute("msg", gno);
+        return "redirect:/room/list";
     }
 
 
