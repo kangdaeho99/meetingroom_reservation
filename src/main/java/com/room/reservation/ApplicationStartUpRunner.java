92eecc2c6f402d92573823c422a33a7b100819df
daeho kang <eoghdhdls@naver.com>
2023-04-01 21:28:06 +0900

    [EDIT] Spring ApplicationRunner를 Error Creating Bean 에러로 Repository 대신 Service를 사용하여 데이터 초기값 설정 주석처리

--- src/main/java/com/room/reservation/ApplicationStartUpRunner.java
+++ src/main/java/com/room/reservation/ApplicationStartUpRunner.java
@@ -7,25 +7,13 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-//@DependsOn("RoomRepository")
 public class ApplicationStartUpRunner implements ApplicationRunner {
-
-//    @Autowired
-//    private RoomRepository roomRepository;
 
     @Autowired
     private RoomService roomService;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-//        IntStream.rangeClosed(1, 24).forEach(i ->{
-//            Room room = Room.builder()
-//                    .title("Title...."+i)
-//                    .content("Content...."+i)
-//                    .writer("user..."+i)
-//                    .build();
-//            roomRepository.save(room);
-//        });
 
         roomService.initRoomDataBase();
 
