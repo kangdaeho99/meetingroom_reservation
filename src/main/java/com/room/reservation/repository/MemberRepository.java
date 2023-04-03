a6ff7858f3c3abef3eaf0103451ded579adbb9fd
daeho kang <eoghdhdls@naver.com>
2023-04-04 06:53:42 +0900

    [ADD] Member, Board, Reply Entity, Repository 생성완료

---
+++ src/main/java/com/room/reservation/repository/MemberRepository.java
@@ -0,0 +1,7 @@
+package com.room.reservation.repository;
+
+import com.room.reservation.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, String> {
+}
