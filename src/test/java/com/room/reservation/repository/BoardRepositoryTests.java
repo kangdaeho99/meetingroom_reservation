ff934c7851ae0a683e8de1f29ef3cbf9acde0176
daeho kang <eoghdhdls@naver.com>
2023-04-05 07:00:15 +0900

    [ADD] Board, Member, Reply 테스트 작성완료

--- src/test/java/com/room/reservation/repository/BoardRepositoryTests.java
+++ src/test/java/com/room/reservation/repository/BoardRepositoryTests.java
@@ -1,4 +1,66 @@
 package com.room.reservation.repository;
 
+
+import com.room.reservation.entity.Board;
+import com.room.reservation.entity.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+/**
+ *
+ * Description :
+ *
+ */
+@SpringBootTest
 public class BoardRepositoryTests {
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
+    /**
+     * 한명의 사용자가 하나의 게시물을 등록하도록 합니다.
+     *
+     */
+    @Test
+    public void insertBoard(){
+        IntStream.rangeClosed(1, 33).forEach(i -> {
+            Member member = Member.builder()
+                    .email("user"+i+"@aaa.com")
+                    .build();
+
+            memberRepository.save(member);
+
+            Board board = Board.builder()
+                    .title("Title..."+i)
+                    .content("Content..."+i)
+                    .writer(member)
+                    .build();
+
+            boardRepository.save(board);
+
+        });
+    }
+
+    /**
+     * Member를 @ManyToOne으로 참조하고 있는 Board를 조회합니다.
+     */
+    @Test
+    public void testRead1(){
+
+        insertBoard();
+
+        Optional<Board> result = boardRepository.findById(25L);
+        if(result.isPresent()){
+            Board board = result.get();
+            System.out.println(board);
+            System.out.println(board.getWriter());
+        }
+
+    }
 }
--- src/test/java/com/room/reservation/repository/MemberRepositoryTests.java
+++ src/test/java/com/room/reservation/repository/MemberRepositoryTests.java
@@ -8,6 +8,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.stream.IntStream;
 
+/**
+ *
+ * Description :
+ * 현재 Member, Board, Reply 3개의 테이블이 PK와 FK의 관계.
+ * PK쪽에서부터 시작하기 위해 Member 먼저 시작합니다.
+ *
+ */
 @SpringBootTest
 public class MemberRepositoryTests {
     @Autowired
@@ -15,7 +22,7 @@ public class MemberRepositoryTests {
 
     @Test
     public void insertMembers(){
-        IntStream.rangeClosed(1, 100).forEach( i -> {
+        IntStream.rangeClosed(1, 32).forEach( i -> {
             Member member = Member.builder()
                     .email("user"+i+"@aaa.com")
                     .password("1111")
