517b92909fda470dbe5f1f5e7643233a17820799
daeho kang <eoghdhdls@naver.com>
2023-04-23 04:28:43 +0900

    [ADD] Server 시작시 세팅 추가

--- src/main/java/com/room/reservation/ApplicationStartUpRunner.java
+++ src/main/java/com/room/reservation/ApplicationStartUpRunner.java
@@ -1,5 +1,9 @@
 package com.room.reservation;
 
+import com.room.reservation.service.MemberService;
+import com.room.reservation.service.ReviewService;
+import com.room.reservation.service.RoomService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -7,7 +11,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class ApplicationStartUpRunner implements ApplicationRunner {
 
+    @Autowired
+    private RoomService roomService;
+
+    @Autowired
+    private MemberService memberService;
+
+    @Autowired
+    private ReviewService reviewService;
+
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        roomService.initDataBase();
+        memberService.initDataBase();
+        reviewService.initDataBase();
     }
 }
