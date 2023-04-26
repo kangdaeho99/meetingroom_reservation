9facde23c9d73929dce7b36871559bae958b1c7f
daeho kang <eoghdhdls@naver.com>
2023-04-27 07:00:23 +0900

    [EDIT] 불필요코드 주석처리

--- build/resources/main/READNOTE/BackUpProject/test-reservation/service/ReplyServiceTests.java
+++ build/resources/main/READNOTE/BackUpProject/test-reservation/service/ReplyServiceTests.java
@@ -1,20 +1,20 @@
-package com.room.reservation.service;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.List;
-
-@SpringBootTest
-public class ReplyServiceTests {
-    @Autowired
-    private ReplyService service;
-
-    @Test
-    public void testGetList(){
-        Long bno = 5L;
-        List<ReplyDTO> replyDTOList = service.getList(bno);
-        replyDTOList.forEach(replyDTO -> System.out.println(replyDTO));
-    }
-}
+//package com.room.reservation.service;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import java.util.List;
+//
+//@SpringBootTest
+//public class ReplyServiceTests {
+//    @Autowired
+//    private ReplyService service;
+//
+//    @Test
+//    public void testGetList(){
+//        Long bno = 5L;
+//        List<ReplyDTO> replyDTOList = service.getList(bno);
+//        replyDTOList.forEach(replyDTO -> System.out.println(replyDTO));
+//    }
+//}
