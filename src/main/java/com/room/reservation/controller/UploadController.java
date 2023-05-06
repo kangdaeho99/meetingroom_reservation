a8a2c61d373cdcc3b5fb33a6fd45da4742ec3caf
daeho kang <eoghdhdls@naver.com>
2023-05-06 12:35:02 +0900

    [ADD] Thumbnailator를 활용하여 이미지 업로드

--- src/main/java/com/room/reservation/controller/UploadController.java
+++ src/main/java/com/room/reservation/controller/UploadController.java
@@ -2,6 +2,7 @@ package com.room.reservation.controller;
 
 import com.room.reservation.dto.UploadResultDTO;
 import lombok.extern.log4j.Log4j2;
+import net.coobird.thumbnailator.Thumbnailator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -55,7 +56,14 @@ public class UploadController {
             Path savePath = Paths.get(saveName);
 
             try{
+                //원본 파일 저장
                 uploadFile.transferTo(savePath); //실제 이미지 저장
+                //섬네일 생성
+                String thumbnailSaveName = uploadPath + File.separator + folderPath + File.separator + "s_" + uuid + "_" + fileName;
+                //섬네일 파일 이름은 중간에 s_ 로 시작하도록
+                File thumbnailFile = new File(thumbnailSaveName);
+                //섬네일 생성
+                Thumbnailator.createThumbnail(savePath.toFile(), thumbnailFile, 100, 100);
                 resultDTOList.add(new UploadResultDTO(fileName, uuid, folderPath));
             } catch(IOException e){
                 e.printStackTrace();
