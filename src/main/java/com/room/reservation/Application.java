403390f4087ebae47a1b49d780bb23e6558017d4
daeho kang <eoghdhdls@naver.com>
2023-06-05 05:41:40 +0900

    feat: H2에서 MariaDB로 Migraiton하면서 오류난 것들 수정진행(/list에서 group by 안되는에러)

--- src/main/java/com/room/reservation/Application.java
+++ src/main/java/com/room/reservation/Application.java
@@ -7,12 +7,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 /**
  *
  * description :
- *
- *@EnableJpaAuditing : JPA Auditing을 활성화시킵니다.
- * @EnableJpaAuditing 를 삭제시킵니다. 이유는 @EnableJpaAuditing을 사용하기 위해서는 최소 하나의 @entity 클래스가 필요한데,
+ *EnableJpaAuditing : JPA Auditing을 활성화시킵니다.
+ * EnableJpaAuditing 를 삭제시킵니다. 이유는 @EnableJpaAuditing을 사용하기 위해서는 최소 하나의 @entity 클래스가 필요한데,
  * 테스트 단에서는 @WebMvcTest이므로 당연히 없습니다. @EnableJpaAuditing가 SpringBootApplication과 함께 있다보니
- * @WebMvcTest에서도 스캔하게 도ㅓㅣ었습니다. 그래서 @EnableJpaAuditing과 @SpringBootApplication을 둘을 분리합니다.
- * @SpringBootApplication : 스프링부트의 자동설정, 스프링 Bean 읽기와 생성
+ * WebMvcTest 에서도 스캔하게 되었습니다. 그래서 @EnableJpaAuditing과 @SpringBootApplication을 둘을 분리합니다.
+ * SpringBootApplication : 스프링부트의 자동설정, 스프링 Bean 읽기와 생성
  * SpringBootApplication이 있는 위치부터 설정을 읽어가기 때문에 이 클래스는 항상 프로젝트의 최상단에 위치해야합니다.
  *
  */
