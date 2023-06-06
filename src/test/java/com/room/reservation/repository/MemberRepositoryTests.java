bd5ccdd686c3d3776d1a3071047bc1e422fe3704
daeho kang <eoghdhdls@naver.com>
2023-06-06 16:31:14 +0900

    feat: Spring SEcurity 관련 설정, PWEncoder, memberRole Setting, Read Test

--- src/test/java/com/room/reservation/repository/MemberRepositoryTests.java
+++ src/test/java/com/room/reservation/repository/MemberRepositoryTests.java
@@ -2,12 +2,16 @@ package com.room.reservation.repository;
 
 
 import com.room.reservation.entity.Member;
+import com.room.reservation.entity.MemberRole;
 import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.annotation.Commit;
 
+import java.util.Optional;
 import java.util.stream.IntStream;
 
 @SpringBootTest
@@ -18,14 +22,26 @@ public class MemberRepositoryTests {
     @Autowired
     private ReviewRepository reviewRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @DisplayName("Member 테스트데이터 삽입")
     @Test
     public void insertMembers(){
-        IntStream.rangeClosed(1, 15).forEach(i ->{
+
+        //1 ~ 10까지는 USER
+        //11 ~ 20까지는 USER, ADMIN
+        IntStream.rangeClosed(1, 20).forEach(i ->{
             Member member = Member.builder()
-                    .email("r"+i+"@hello.com")
-                    .pw("1111")
-                    .nickname("reviewer"+i)
+                    .email("user"+i+"@hello.com")
+                    .password(passwordEncoder.encode("1111"))
+                    .nickname("Member"+i)
                     .build();
+
+            //default Role
+            member.addMemberRole(MemberRole.USER);
+
+            if( i > 10) member.addMemberRole(MemberRole.ADMIN);
+
             memberRepository.save(member);
         });
     }
@@ -45,4 +61,13 @@ public class MemberRepositoryTests {
 //        reviewRepository.deleteByMember(member);
 //        memberRepository.deleteById(mno);
     }
+
+    @DisplayName("Member 데이터 조회")
+    @Test
+    public void testRead(){
+        Optional<Member> result = memberRepository.findByEmail("user1@hello.com", false);
+        Member member = result.get();
+        System.out.println(member);
+    }
+
 }
